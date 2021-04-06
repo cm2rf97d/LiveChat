5f4632be61594bee2f2610deb08efab6cba11f40
Mike <cm2rf97d@gmail.com>
2021-04-06 16:01:38 +0800

    add Sign Up View

--- LiveChat/Control/HomepageViewController.swift
+++ LiveChat/Control/HomepageViewController.swift
@@ -7,17 +7,41 @@
 
 import UIKit
 
-class HomepageViewController: UIViewController
+class HomepageViewController: UIViewController,UITextFieldDelegate
 {
     let homepageview = HomePageView()
+    // Keep Keyboard Height
+    var keyboardHeight: CGFloat = 0
+    weak var scrollView: UIScrollView!
     
+    // MARK: - Life Cycle
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.barTintColor = .black
+        homepageview.accountTextField.delegate = self
+        homepageview.passwordTextField.delegate = self
     }
-    
+
     override func loadView()
     {
         self.view = homepageview
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        textField.resignFirstResponder()
+        return true
     }
 }
--- LiveChat/View/HomePageView.swift
+++ LiveChat/View/HomePageView.swift
@@ -6,17 +6,103 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomePageView: UIView
 {
+    // MARK: - IBOutlet
+    let titleLabel: UILabel =
+    {
+        let label = UILabel()
+        label.text = "IC"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 50)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    let accountTextField: UITextField =
+    {
+        let field = UITextField()
+        field.backgroundColor = .systemGray3
+        field.borderStyle = .roundedRect
+        field.clearButtonMode = .whileEditing
+        field.keyboardType = .emailAddress
+        field.placeholder = " account"
+        return field
+    }()
+    
+    let passwordTextField: UITextField =
+    {
+        let field = UITextField()
+        field.backgroundColor = .systemGray3
+        field.borderStyle = .roundedRect
+        field.clearButtonMode = .whileEditing
+        field.keyboardType = .emailAddress
+        field.placeholder = " password"
+        return field
+    }()
+    
+    let signUpButton: UIButton =
+    {
+        let button = UIButton()
+        button.setTitle("Go", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.setTitleColor(.black, for: .highlighted)
+        return button
+    }()
+
+    // MARK: - Init
     override init(frame: CGRect)
     {
         super.init(frame: frame)
-        self.backgroundColor = .systemBlue
+        self.backgroundColor = .black
+        addSubview(titleLabel)
+        addSubview(accountTextField)
+        addSubview(passwordTextField)
+        addSubview(signUpButton)
+        autoLayout()
     }
     
     required init?(coder: NSCoder)
     {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Layout
+    func autoLayout()
+    {
+        titleLabel.snp.makeConstraints
+        { (make) in
+            make.centerX.equalTo(self)
+            make.top.equalTo(150)
+            make.width.equalTo(UIScreen.main.bounds.width * 0.5)
+            make.height.equalTo(70)
+        }
+        
+        accountTextField.snp.makeConstraints
+        { (make) in
+            make.centerX.equalTo(self)
+            make.top.equalTo(350)
+            make.width.equalTo(UIScreen.main.bounds.width * 0.7)
+            make.height.equalTo(30)
+        }
+        
+        passwordTextField.snp.makeConstraints
+        { (make) in
+            make.centerX.equalTo(self)
+            make.top.equalTo(400)
+            make.width.equalTo(UIScreen.main.bounds.width * 0.7)
+            make.height.equalTo(30)
+        }
+        
+        signUpButton.snp.makeConstraints
+        { (make) in
+            make.centerX.equalTo(self)
+            make.top.equalTo(600)
+            make.width.equalTo(100)
+            make.height.equalTo(30)
+        }
+
     }
 }
