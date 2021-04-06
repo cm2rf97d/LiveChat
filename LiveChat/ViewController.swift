577f9ab6790b5a8c223c55011de178dced266e0a
Mike <cm2rf97d@gmail.com>
2021-04-06 11:15:07 +0800

    first commit

---
+++ LiveChat/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  LiveChat
+//
+//  Created by 陳郁勳 on 2021/4/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
